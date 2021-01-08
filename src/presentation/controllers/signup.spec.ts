ee5a822ceefca6d286327998523e96a16d56a9f3
Douglas Korgut <douglaskorgutt@gmail.com>
2021-01-08 18:20:26 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,17 +1,18 @@
 import { SignUpController } from './signup'
+import { HttpRequest, HttpResponse } from '../protocols/http'
 
 describe('SignUp Controller', () => {
   test('should return 400 if no nome is provided', () => {
     // sut = system under test
     const sut = new SignUpController()
-    const httpRequest = {
+    const httpRequest: HttpRequest = {
       body: {
         email: 'email@gmail.com',
         password: 'password',
         passwordConfirmation: 'passwordConfirmation'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse: HttpResponse = sut.handle(httpRequest)
     // toBe expect to identical objects
     expect(httpResponse.statusCode).toBe(400)
     // toEqual expect identical values
@@ -21,14 +22,14 @@ describe('SignUp Controller', () => {
   test('should return 400 if no email is provided', () => {
     // sut = system under test
     const sut = new SignUpController()
-    const httpRequest = {
+    const httpRequest: HttpRequest = {
       body: {
-        name: 'any_name@gmail.com',
+        name: 'any_name',
         password: 'password',
         passwordConfirmation: 'passwordConfirmation'
       }
     }
-    const httpResponse = sut.handle(httpRequest)
+    const httpResponse: HttpResponse = sut.handle(httpRequest)
     // toBe expect to identical objects
     expect(httpResponse.statusCode).toBe(400)
     // toEqual expect identical values
