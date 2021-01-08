a71cacb1d71c4b68e67c0dc25fac792efc3b0bb8
Douglas Korgut <douglaskorgutt@gmail.com>
2021-01-08 17:28:11 -0300

    test: ensure SignUpController returns an error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -17,4 +17,21 @@ describe('SignUp Controller', () => {
     // toEqual expect identical values
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+
+  test('should return 400 if no email is provided', () => {
+    // sut = system under test
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name@gmail.com',
+        password: 'password',
+        passwordConfirmation: 'passwordConfirmation'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    // toBe expect to identical objects
+    expect(httpResponse.statusCode).toBe(400)
+    // toEqual expect identical values
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
