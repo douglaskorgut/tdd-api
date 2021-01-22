68b9563aa855b9b6bb31bb896fd8e2da24735062
douglaskorgut <douglas.korgut@ubivis.io>
2021-01-22 11:25:33 -0300

    chore: jest configs update

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
-export default {
+module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: "coverage",
   testEnvironment: "node",
   transform: {
     '.+\\.ts$': 'ts-jest'
-  },
-};
+  }
+}
